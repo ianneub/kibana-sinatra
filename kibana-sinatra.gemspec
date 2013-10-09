7bb1cf1b3a8da5ca5da9c446f8f19a477f8391e9
Ian Neubert <ian@ianneubert.com>
2013-10-09 07:55:09 -0700

    Updated to latest kibana 3 master - 1fba324e971a7ae1499f3c1ba2c71e821f556c31

--- kibana-sinatra.gemspec
+++ kibana-sinatra.gemspec
@@ -20,5 +20,7 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
+  spec.add_development_dependency "uuid"
+  spec.add_development_dependency "rubyzip"
   spec.add_dependency "sinatra", "~> 1.4"
 end
---
+++ lib/tasks/update.rb
@@ -0,0 +1,51 @@
+require 'fileutils'
+require 'open-uri'
+require 'uuid'
+require 'zip'
+
+desc "Update kibana from upstream"
+task :update do
+  asset_path = File.expand_path(File.dirname(__FILE__) + "/../kibana/assets")
+  download_url = 'https://github.com/elasticsearch/kibana/archive/master.zip'
+  download_file = "/tmp/#{UUID.new.generate}_master.zip"
+  tmp_path = "/tmp/#{UUID.new.generate}"
+  
+  # Remove kibana assets
+  FileUtils.rm_rf(asset_path)
+
+  # Create the tmp folder
+  FileUtils.mkdir(tmp_path)
+
+  # Download latest kibana from github
+  File.open(download_file, "wb") do |file|
+    file.write open(download_url).read
+  end
+
+  # Unpack zip into tmp folder
+  unzip_file download_file, tmp_path
+
+  # Copy src contents to asset folder
+  FileUtils.mv "#{tmp_path}/kibana-master/src", asset_path
+
+  # Delete tmp folder and downloaded file
+  FileUtils.rm_rf(tmp_path)
+  FileUtils.rm(download_file)
+
+  # Move the config.js file to the view file and setup erb variables
+  config_file = "#{asset_path}/../views/config.erb"
+  FileUtils.mv "#{asset_path}/config.js", config_file
+  text = File.read(config_file)
+  text.gsub!('http://"+window.location.hostname+":9200', '<%= Kibana::Sinatra::Web.elasticsearch_url %>')
+  text.gsub!('"kibana-int"', '"<%= Kibana::Sinatra::Web.kibana_index %>"')
+  File.open(config_file, "w") {|file| file.write(text) }
+end
+
+def unzip_file(file, destination)
+  Zip::File.open(file) { |zip_file|
+   zip_file.each { |f|
+     f_path=File.join(destination, f.name)
+     FileUtils.mkdir_p(File.dirname(f_path))
+     zip_file.extract(f, f_path) unless File.exist?(f_path)
+   }
+  }
+end
